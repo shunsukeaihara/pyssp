6808044a11d72cfad57b95ee96c54729ec54bdb9
aihara <devnull@localhost>
2011-07-06 16:26:40 +0900

    itakura-saito distaceに関してテスト以外を追加

--- distance.py
+++ distance.py
@@ -3,19 +3,43 @@
 import scipy as sp
 from util import get_frame
 
-def itakura_saito_distotion_measure(s,s_hat):
-    size = min(len(s),len(s_hat))
+def itakura_saito_distotion_measure(s,shat):
+    size = min(len(s),len(shat))
     s = s[0:size]
-    s_hat = s[0:size]
-    return sp.sum(s - s_hat + sp.exp(s_hat)/sp.exp(s)-1.0)/size
+    shat = shat[0:size]
+    return sp.sum(s - shat + sp.exp(shat)/sp.exp(s)-1.0)/float(size)
 
 
-def segmental_itakura_saito_distotion_measure(s,s_hat,winsize):
-    size = min(len(s),len(s_hat))
+def segmental_itakura_saito_distotion_measure(s,shat,winsize):
+    size = min(len(s),len(shat))
     nf = size/(winsize/2) - 1
     ret=[]
     for no in xrange(nf):
         s_i = get_frame(s, winsize, no)
-        s_hat_i = get_frame(s_hat, winsize, no)
-        ret.append(itakura_saito_distotion_measure(s_i,s_hat_i))
+        shat_i = get_frame(shat, winsize, no)
+        ret.append(itakura_saito_distotion_measure(s_i,shat_i))
     return ret
+
+
+def itakura_saito_spectrum_distance(s,shat,winfunc):
+    size = min(len(s),len(shat))
+    window = winfunc(size)
+    s = s[0:size]
+    shat = shat[0:size]
+    s_amp = sp.absolute(sp.fft(s*window))
+    shat_amp = sp.absolute(sp.fft(shat*window))
+    return sp.mean(s_amp / shat_amp - sp.log10(s_amp / shat_amp) - 1.0)
+
+
+def segmental_itakura_saito_spectrum_distance(s,shat,winsize,winfunc):
+    size = min(len(s),len(shat))
+    nf = size/(winsize/2) - 1
+    ret=[]
+    for no in xrange(nf):
+        s_i = get_frame(s, winsize, no)
+        shat_i = get_frame(shat, winsize, no)
+        ret.append(itakura_saito_spectrum_distance(s_i,shat_i))
+    return ret
+
+if __name__=="__main__":
+    pass
