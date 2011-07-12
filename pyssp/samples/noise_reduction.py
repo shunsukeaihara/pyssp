5fe82d9dd60e5739e89bd43ffbcc7641ea50238a
aihara <devnull@localhost>
2011-07-12 20:01:15 +0900

    標準入力からwaveを受け取って、標準出力にwaveを吐き出すようにサンプルを変更

--- pyssp/samples/noise_reduction.py
+++ pyssp/samples/noise_reduction.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import scipy as sp
-from pyssp.util import read_signal,get_frame,add_signal,write_signal,separate_channels,uniting_channles,compute_avgpowerspectrum,compute_avgamplitude
+import sys
+import wave
+import tempfile
+from pyssp.util import read_signal,get_frame,add_signal,separate_channels,uniting_channles,compute_avgpowerspectrum
 from pyssp.voice_enhancement import SupectralSubtruction,MMSE_STSA,JointMap,MMSE_LogSTSA
 import optparse
 
@@ -9,15 +12,40 @@ WINSIZE=1024
 
 def noise_reduction(signal,winsize,window,ss):
     out=sp.zeros(len(signal),sp.float32)
-    #n_pow = compute_avgamplitude(signal[0:winsize*5],winsize,window)**2.0
     n_pow = compute_avgpowerspectrum(signal[0:winsize*10],winsize,window)
     for no in xrange(nf):
         s = get_frame(signal, winsize, no)
         add_signal(out, ss.compute_by_noise_pow(s,n_pow), winsize, no)
     return out
 
+
+def write(param,signal):
+    st = tempfile.TemporaryFile()
+    wf=wave.open(st,'wb')
+    wf.setparams(params)
+    s=sp.int16(signal).tostring()
+    wf.writeframes(s)
+    st.seek(0)
+    print st.read()
+
+def read(fname,winsize):
+    if fname =="-":
+        wf=wave.open(sys.stdin,'rb')
+        n=wf.getnframes()
+        str=wf.readframes(n)
+        params = ((wf.getnchannels(), wf.getsampwidth(),
+                   wf.getframerate(), wf.getnframes(),
+                   wf.getcomptype(), wf.getcompname()))
+        siglen=((int )(len(str)/2/winsize) + 1) * winsize
+        signal=sp.zeros(siglen, sp.int16)
+        signal[0:len(str)/2] = sp.fromstring(str,sp.int16)
+        return signal,params
+    else:
+        return read_signal(fname,winsize)
+
+
 if __name__=="__main__":
-    parser = optparse.OptionParser(usage="%prog [-m METHOD] [-w WINSIZE] INPUTFILE\n method 0 : SupectralSubtruction\n        1 : MMSE_STSA\n        2 : MMSE_LogSTSA\n        3 : JointMap")
+    parser = optparse.OptionParser(usage="%prog [-m METHOD] [-w WINSIZE] INPUTFILE\n method 0 : SupectralSubtruction\n        1 : MMSE_STSA\n        2 : MMSE_LogSTSA\n        3 : JointMap\n if INPUTFILE is \"-\", read wave data from stdin")
 
     parser.add_option("-w", type="int", dest="winsize", default=WINSIZE)
     parser.add_option("-m", type="int", dest="method", default=0)
@@ -27,10 +55,11 @@ if __name__=="__main__":
     if len(args)!=1:
         parser.print_help()
         exit(2)
-    
-    signal, params = read_signal(args[0],options.winsize)
-    nf = len(signal)/(options.winsize/2) - 1
 
+    fname = args[0]
+    signal, params = read(fname,options.winsize)
+
+    nf = len(signal)/(options.winsize/2) - 1
     window = sp.hanning(options.winsize)
     import os.path
     
@@ -49,7 +78,7 @@ if __name__=="__main__":
         outfname = "%s_jm%s" % (root,ext)
 
     if params[0]==1:
-        write_signal(outfname, params, noise_reduction(signal,options.winsize,window,ss))
+        write(params, noise_reduction(signal,options.winsize,window,ss))
     elif params[0]==2:
         l,r = separate_channels(signal)
-        write_signal(outfname, params, uniting_channles(noise_reduction(l,options.winsize,window,ss),noise_reduction(r,options.winsize,window,ss)))
+        write(params, uniting_channles(noise_reduction(l,options.winsize,window,ss),noise_reduction(r,options.winsize,window,ss)))
--- pyssp/samples/vad.py
+++ pyssp/samples/vad.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 import scipy as sp
 import sys
-import os
-from pyssp.util import get_frame,add_signal,read_signal,write_signal,separate_channels,uniting_channles
+import wave
+import tempfile
+from pyssp.util import get_frame,add_signal,read_signal,separate_channels,uniting_channles
 from pyssp.vad.ltsd import LTSD
+import optparse
+
+WINSIZE = 1024
 
 def vad(vas,signal,winsize,window):
     out=sp.zeros(len(signal),sp.float32)
@@ -13,24 +17,55 @@ def vad(vas,signal,winsize,window):
             add_signal(out,get_frame(signal, winsize, i)*window,winsize,i)
     return out
 
+def write(param,signal):
+    st = tempfile.TemporaryFile()
+    wf=wave.open(st,'wb')
+    wf.setparams(params)
+    s=sp.int16(signal).tostring()
+    wf.writeframes(s)
+    st.seek(0)
+    print st.read()
+
+def read(fname,winsize):
+    if fname =="-":
+        wf=wave.open(sys.stdin,'rb')
+        n=wf.getnframes()
+        str=wf.readframes(n)
+        params = ((wf.getnchannels(), wf.getsampwidth(),
+                   wf.getframerate(), wf.getnframes(),
+                   wf.getcomptype(), wf.getcompname()))
+        siglen=((int )(len(str)/2/winsize) + 1) * winsize
+        signal=sp.zeros(siglen, sp.int16)
+        signal[0:len(str)/2] = sp.fromstring(str,sp.int16)
+        return signal,params
+    else:
+        return read_signal(fname,winsize)
+
+    
 if __name__ == "__main__":
     """
     python vad.py WINSIZE THREATHOLD FILENAME
     """
-    windowsize = int(sys.argv[1])
-    signal, params = read_signal(sys.argv[3],windowsize)
-    root,ext = os.path.splitext(sys.argv[3])
-    outfname = "%s_vaded%s" % (root,ext)
+    parser = optparse.OptionParser(usage="%python vad [-t THREASHOLD] [-w WINSIZE] INPUTFILE \n if INPUTFILE is \"-\", read wave data from stdin")
+
+    parser.add_option("-w", type="int", dest="winsize", default=WINSIZE)
+    parser.add_option("-t", type="int", dest="th", default=15)
+
+    (options, args) = parser.parse_args()
+    windowsize = options.winsize
+
+    fname = args[0]
+    signal, params = read(fname,options.winsize)
     window = sp.hanning(windowsize)
 
     if params[0]==1:
-        ltsd = LTSD(windowsize,window,5,lambda0=int(sys.argv[2]))
+        ltsd = LTSD(windowsize,window,5,lambda0=options.th)
         res,ltsds =  ltsd.compute_without_noise(signal)
-        write_signal(outfname,params,vad(res,signal,windowsize,window))
+        write(params,vad(res,signal,windowsize,window))
     elif params[0]==2:
         l,r = separate_channels(signal)
-        ltsd_l = LTSD(windowsize,window,5,lambda0=int(sys.argv[2]))
-        ltsd_r = LTSD(windowsize,window,5,lambda0=int(sys.argv[2]))
+        ltsd_l = LTSD(windowsize,window,5,lambda0=options.th)
+        ltsd_r = LTSD(windowsize,window,5,lambda0=options.th)
         out = uniting_channles(vad(ltsd_l.compute_without_noise(l)[0],l,windowsize,window),
                                vad(ltsd_r.compute_without_noise(r)[0],r,windowsize,window))
-        write_signal(outfname,params,out)
+        write(params,out)
